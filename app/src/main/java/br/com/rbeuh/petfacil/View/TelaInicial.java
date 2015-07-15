5d9d42fbca3d144c353fefae30da8f4c279260cf
enzotiezzi <enzo_tiezzi@hotmail.com>
2015-07-15 16:11:01 -0300

    Adicionando estrutura de pastas

--- app/src/main/java/br/com/rbeuh/petfacil/View/TelaInicial.java
+++ app/src/main/java/br/com/rbeuh/petfacil/View/TelaInicial.java
@@ -1,9 +1,11 @@
-package br.com.rbeuh.petfacil;
+package br.com.rbeuh.petfacil.View;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import br.com.rbeuh.petfacil.R;
 
 
 public class TelaInicial extends ActionBarActivity
