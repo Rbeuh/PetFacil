6725bddd30d3e707da3a0f134ec46cc672f7757a
enzotiezzi <enzo_tiezzi@hotmail.com>
2015-07-17 19:59:25 -0300

    adicionando nas pastas corretas

--- app/src/main/java/br/com/rbeuh/petfacil/Fragments/NavigationDrawerFragment.java
+++ app/src/main/java/br/com/rbeuh/petfacil/Fragments/NavigationDrawerFragment.java
@@ -1,4 +1,4 @@
-package br.com.rbeuh.petfacil;
+package br.com.rbeuh.petfacil.Fragments;
 
 import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
@@ -21,6 +21,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import br.com.rbeuh.petfacil.R;
 
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
